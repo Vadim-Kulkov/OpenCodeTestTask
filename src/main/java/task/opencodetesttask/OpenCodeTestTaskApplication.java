be500a7fd8584f57eb371579df69aaf36a132d9f
Vadim <manavar2017@yandex.com>
2023-04-09 22:51:28 +0400

    init addition

---
+++ src/main/java/task/opencodetesttask/OpenCodeTestTaskApplication.java
@@ -0,0 +1,13 @@
+package task.opencodetesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCodeTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenCodeTestTaskApplication.class, args);
+    }
+
+}
